e067d7d33f8d0e965a11f989ec9e610ae57f3f0e
Chilishikawa <chilishikawa13@gmail.com>
2025-04-21 03:22:58 -0400

    first commit

--- inicializar_repo.sh
+++ inicializar_repo.sh
@@ -7,6 +7,6 @@ git add README.md
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/Chilishikawa/Familia-12.git
+git remote add origin https://github.com/Chilishikawa/Familia-11.git
 
 echo "✅ Repositorio inicializado y configurado con GitHub: Familia-11"
